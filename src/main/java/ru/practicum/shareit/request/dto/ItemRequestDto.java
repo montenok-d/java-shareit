18052822881449de1962722c3b705194c718883b
montenok <montenok-d@yandex.ru>
2024-09-17 22:03:30 +0300

    Style fix

--- src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
+++ src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
@@ -3,13 +3,10 @@ package ru.practicum.shareit.request.dto;
 import lombok.Builder;
 import lombok.Data;
 
-import java.time.LocalDateTime;
-
 @Data
 @Builder
 public class ItemRequestDto {
     private long id;
     private String description;
     private long requestor;
-    //private LocalDateTime created;
 }
